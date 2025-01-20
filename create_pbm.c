eb1d392868eb7837130df56736488deedefc9c06
Fernando Iglessias <figlessias@gmail.com>
2025-01-19 22:07:39 -0300

    start

---
+++ create_pbm.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int identifier[8];
+
+    for (int i = 0; i < 8; i++) {
+        
+        scanf("%d", &numeros[i]) // Input do identificador
+
+    }
+
+    
+ 
+    return 0;
+}
